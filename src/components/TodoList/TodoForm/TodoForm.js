35ca352b67565289082945b7a9bf8866d04719b3
stilet00 <safroninanton@gmail.com>
2021-05-14 13:09:48 +0300

    Formik added

--- src/components/TodoList/TodoForm/TodoForm.js
+++ src/components/TodoList/TodoForm/TodoForm.js
@@ -2,11 +2,12 @@ import React, { useCallback, useState } from 'react';
 import { FormControl, FormGroup, InputLabel, Select, TextField } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import SaveRoundedIcon from "@material-ui/icons/SaveRounded";
-import { Link, useHistory, useParams, useRouteMatch } from "react-router-dom";
-import ArrowBackRoundedIcon from "@material-ui/icons/ArrowBackRounded";
+import CloseIcon from '@material-ui/icons/Close';
+import { Link, useHistory } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import { addTodo } from "../../../store/todos/thunks";
 import { connect } from "react-redux";
+import { Formik } from 'formik';
 const useStyles = makeStyles((theme) => ({
     formControl: {
         margin: theme.spacing(1),
@@ -19,53 +20,45 @@ const useStyles = makeStyles((theme) => ({
 function TodoForm ({  addTodo }) {
     const classes = useStyles();
     const history = useHistory();
-    const [todo, setTodo] = useState({title: '', isDone: false});
-    let onInputChange = useCallback(
-        (e) => {
-            setTodo({ ...todo, [e.target.name]: e.target.value.trim() });
-        },
-        [todo, setTodo]
-    );
     return (
-        <form action="" onSubmit={(e) => {
-            e.preventDefault();
-            addTodo(todo);
-            history.push("/todos");
-        }
-        }>
-            <FormGroup className={"container"}>
-                <TextField id="outlined-basic" label="Title:" variant="outlined" type="text"
-                           value={todo.title}
-                           name={"title"}
-                           onChange={onInputChange}
-                />
-                <FormControl variant="outlined" className={classes.formControl}>
-                    <InputLabel htmlFor="outlined-age-native-simple">Status:</InputLabel>
-                    <Select
-                        native
-                        value={todo.isDone}
-                        onChange={(e) => setTodo({...todo, isDone: e.target.value})}
-                        label="Status:"
-                        inputProps={{
-                            name: 'age',
-                            id: 'outlined-age-native-simple',
-                        }}
-                    >
-                        <option aria-label="None" value="" />
-                        <option value={true}>Done</option>
-                        <option value={false}>Not done</option>
-                    </Select>
-                </FormControl>
-                <Button variant="contained" type={"submit"}>
-                    <SaveRoundedIcon />
-                </Button>
-                <Button variant="contained" type={"button"}>
-                    <Link to={"/todos"} className={"button-inner"}>
-                        <ArrowBackRoundedIcon />
-                    </Link>
-                </Button>
-            </FormGroup>
-        </form>
+        <Formik initialValues={{title: '', isDone: false}} onSubmit={(values) => {
+            addTodo(values);
+            history.push('todos/')
+        } }>
+            { (formik) => { return (
+                <form action="" onSubmit={formik.handleSubmit}>
+                    <FormGroup className={"container"}>
+                        <TextField id="outlined-basic" label="Title:" variant="outlined" type="text"
+                                   value={formik.values.title}
+                                   name={"title"}
+                                   onChange={formik.handleChange}
+                        />
+                        <FormControl variant="outlined" className={classes.formControl}>
+                            <InputLabel>Status:</InputLabel>
+                            <Select
+                                native
+                                value={formik.values.isDone}
+                                onChange={formik.handleChange}
+                                label="Status:"
+                                name={'isDone'}
+                            >
+                                <option value={true}>Done</option>
+                                <option value={false}>Not done</option>
+                            </Select>
+                        </FormControl>
+                        <Button variant="contained" type={"submit"} disabled={!formik.isValid}>
+                            <SaveRoundedIcon />
+                        </Button>
+                        <Button variant="contained" type={"button"}>
+                            <Link to={"/todos"} className={"button-inner"}>
+                                <CloseIcon />
+                            </Link>
+                        </Button>
+                    </FormGroup>
+                </form>
+            )}
+            }
+        </Formik>
     );
 }
 const mapStateToProps = ({ todos }) => ({ todos: todos.todos });
